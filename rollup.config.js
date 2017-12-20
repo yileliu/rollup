c2bf3dd057b562ab2b2706093727f00e41fd382f
Yile Liu <yile.liu@autodesk.com>
2017-12-20 23:25:46 +0800

    try more plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     input: 'src/main.js',
@@ -6,5 +8,8 @@ export default {
         file: 'dist/bundle.js',
         format: 'cjs'
     },
-    plugins: [json()]
+    plugins: [commonjs({
+        include: 'node_modules/**'
+    }), json(), resolve()],
+    external: ['lodash']
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import foo from './foo.js';
 import { version } from '../package.json';
+import answer from 'the-answer';
+import _ from 'lodash';
+
 export default function () {
   console.log(foo);
   console.log(`version ${version}`);
+  console.log('the answer is ' + answer);
+
+  const sum = _.add(1, 3);
+  console.log(sum);
 }
